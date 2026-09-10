7c6e35fefb3b467564ac4a28f797142234da2b90
tippytaptap <haider194ali@gmail.com>
2026-09-10 19:02:35 +0100

    Route classes and enrolment to dedicated pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ import { standaloneSections } from '@/content/sectionRoutes';
 import MainLayout from '@/layouts/MainLayout';
 
 import AboutPage from '@/pages/AboutPage';
+import ClassesCoursesPage from '@/pages/ClassesCoursesPage';
 import ContactPage from '@/pages/ContactPage';
 import FinancialHistoryPage from '@/pages/FinancialHistoryPage';
 import HallBookingPage from '@/pages/HallBookingPage';
 import HomePage from '@/pages/HomePage';
+import MadrassahEnrolmentPage from '@/pages/MadrassahEnrolmentPage';
 import MadrassahPage from '@/pages/MadrassahPage';
 import NotFoundPage from '@/pages/NotFoundPage';
 import PrayerTimesPage from '@/pages/PrayerTimesPage';
@@ -70,6 +72,8 @@ export default function App() {
           <Route path="services/hall-booking" element={<HallBookingPage />} />
           <Route path="projects" element={<ProjectsPage />} />
           <Route path="madrassah" element={<MadrassahPage />} />
+          <Route path="madrassah/classes-courses" element={<ClassesCoursesPage />} />
+          <Route path="madrassah/enrolment" element={<MadrassahEnrolmentPage />} />
           <Route path="youth" element={<YouthPage />} />
           <Route path="youth/itikaf" element={<YouthPage view="itikaf" />} />
 
