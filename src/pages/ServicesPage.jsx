f189d8ba301ac3ff2b45a261cf5a013272f1be1c
tippytaptap <haider194ali@gmail.com>
2026-09-10 19:02:51 +0100

    Remove redundant services hero

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import HeroSection from '@/components/sections/services/HeroSection';
 import ServiceCategoriesTabs from '@/components/sections/services/ServiceCategoriesTabs';
 import SpecialServicesSection from '@/components/sections/services/SpecialServicesSection';
 
 export default function ServicesPage() {
   return (
     <div className="page-transition">
-      <HeroSection />
       <ServiceCategoriesTabs />
       <SpecialServicesSection />
     </div>
